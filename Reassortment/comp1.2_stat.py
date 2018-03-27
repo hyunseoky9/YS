78adc263f0c00901a9d03b06cd6fee64d25828ba
Hyun Seok Yoon <hy324@cornell.edu>
2018-03-27 15:21:47 +0900

    tracks k and adds destination if not there

--- Reassortment/comp1.2_stat.py
+++ Reassortment/comp1.2_stat.py
@@ -166,12 +166,13 @@ if len(sys.argv) > 1: # input parameters from command line
     except:
         pass
 
-
 start = timeit.default_timer() # timer start
 bar = Bar('Processing', max=rep) # progress bar start
 # write out data with file name indicating time it started collecting
 now = datetime.datetime.now()
 destination = input('Enter file destination?')
+if destination not in os.listdir('./data'):
+    os.command('mkdir ' + destination)
 params = '%d,%d,%.2f,%d,%d,%.5f,%d,%.2f,%.2f,%.2f'%(rep,L,s,N0,K,mu,gen_num,cost,r,N1r)
 file_name = './data/'+destination+'/c1.2.2s_%s(0).csv'%(params)
 while file_name[7::] in os.listdir('./data'):
@@ -179,6 +180,9 @@ while file_name[7::] in os.listdir('./data'):
     file_name = file_name[0:-6] + str(lastnum+1) + file_name[-5::]
 fh = open(file_name,'w')
 detail = input('Do you want to record the population of each time step? 0=no 1=yes (Will only record last step if typed "no")')
+if detail: 
+    detail2 = input('Do you want to record each individual\'s k? 0=no 1=yes (Will only record means if "no"')
+
 if detail:
     fh.write('rep,t,pop1,pop2,k1,k2\n')
     for repe in range(rep):
@@ -203,24 +207,39 @@ if detail:
             viruses1, viruses2 = reproduce(viruses1,viruses2)
 
             # get kmean for each subpop
-            ks = [] # k's for each virus in a subpop
-            if len(viruses1)>0:
-                for i in range(len(viruses1)):
-                    ks.append(viruses1[i].k)
-                k_means1 = np.mean(np.array(ks))
-            else:
-                k_means1 = -1
-            ks = []
-            if len(viruses2)>0:
-                for j in range(len(viruses2)):
-                    ks.append(viruses2[j].k)
-                k_means2 = np.mean(np.array(ks))
-            else:
-                k_means2 = -1
+            if not detail2:
+                ks = [] # k's for each virus in a subpop
+                if len(viruses1)>0:
+                    for i in range(len(viruses1)):
+                        ks.append(viruses1[i].k)
+                    k_means1 = np.mean(np.array(ks))
+                else:
+                    k_means1 = -1
+                ks = []
+                if len(viruses2)>0:
+                    for j in range(len(viruses2)):
+                        ks.append(viruses2[j].k)
+                    k_means2 = np.mean(np.array(ks))
+                else:
+                    k_means2 = -1
 
-            fh.write('%d,%d,%d,%d,%.2f,%.2f\n'%(repe+1,gen+1,len(viruses1),len(viruses2),k_means1, k_means2))
-            N = len(viruses1) + len(viruses2)
-
+                fh.write('%d,%d,%d,%d,%.2f,%.2f\n'%(repe+1,gen+1,len(viruses1),len(viruses2),k_means1, k_means2))
+                N = len(viruses1) + len(viruses2)
+            else:
+                ks1 = [] # k's for each virus in a subpop
+                if len(viruses1)>0:
+                    for i in range(len(viruses1)):
+                        ks1.append(viruses1[i].k)
+                else:
+                    ks1.append('NA')
+                ks2 = []
+                if len(viruses2)>0:
+                    for j in range(len(viruses2)):
+                        ks.append(viruses2[j].k)
+                else:
+                    ks2.append('NA')
+                fh.write('%d,%d,%d,%d,%s,%s\n'%(repe+1,gen+1,len(viruses1),len(viruses2),str(ks1),str(ks2)))
+                N = len(viruses1) + len(viruses2)
         bar.next()
 
 else:
