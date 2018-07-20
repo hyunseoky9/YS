c1fd81fec56f02d34c313308a1e9f4a97e8f1fa5
hyun seok <hy324@cornell.edu>
2018-07-20 14:41:17 +0900

    c

--- Reassortment/command7.2.py
+++ Reassortment/command7.2.py
@@ -12,7 +12,7 @@ back = 0
 timestep = 0
 krecord = 0 #  2= smallest k of the subpop. 1= all indiv's k. 0=mean k.
 untilext = 1 
-rep = 10000
+rep = 10000# 10000
 L = 300
 s = [0.05,0.02]
 N0 = 1000
@@ -22,12 +22,12 @@ gen_num = 1000
 cost = [0.00,0.02,0.04,0.06,0.08,0.1,0.12]
 r = 0.5 
 r2 = 0.75
-N1r = [0.9]#[0.6,0.7,0.8,0.9] #[0.5]
+N1r = [0.6,0.9] #[0.6,0.7,0.8,0.9] #[0.5]
 q = [3]
 a = 0
 b = 0
 type = 1
-destination = 'ultimate_test2'
+destination = 'test'
 file2run = sys.argv[1]
 version = file2run[4:7]
 
@@ -38,13 +38,13 @@ for m in range(len(mu)):
 			for k in range(len(cost)):
 				for l in range(len(q)):
 					if version == '1.2':
-						params = '%s %d %d %d %d %d %d %f %d %d %.5f %d %f %f %f'%(destination,back,timestep,krecord,
+						params = '%s %d %d %d %d %d %f %d %d %.5f %d %f %f %f %d %d %f %f %f %d'%(destination,back,timestep,krecord,
 																				rep,L,s[i],N0,K,mu[m],gen_num,cost[k],r,N1r[j],seed,untilext,q[l],a,b,type)
 					elif version == '1.3':
 						params = '%s %d %d %d %d %d %f %d %d %.5f %d %f %f %f %d %d %f %f %f %d'%(destination,back,timestep,krecord,
 																				rep,L,s[i],N0,K,mu[m],gen_num,cost[k],r,N1r[j],seed,untilext,q[l],a,b,type)
 					elif version == '2.3':
-						params = '%s %d %d %d %d %d %f %d %d %.5f %d %f %f %f %f %f %d'%(destination,back,timestep,krecord,
+						params = '%s %d %d %d %d %d %f %d %d %.5f %d %f %f %f %d %d %f %f %f %d'%(destination,back,timestep,krecord,
 																				rep,L,s[i],N0,K,mu[m],gen_num,cost[k],r,N1r[j],seed,untilext,q[l],a,b,type)
 					if file2run[-1] == "y": # file is .py
 						cd('python %s %s'%(file2run, params))
